566f31852a4ffb49a0ccde59a123f33042dc6fde
Kirill Neskoromnyy <kneskoromny@gmail.com>
2022-10-27 12:48:39 +0300

    feat: launch without storyboard

--- LocalizationTestApp/ViewController.swift
+++ LocalizationTestApp/ViewController.swift
@@ -1,17 +1,11 @@
-//
-//  ViewController.swift
-//  LocalizationTestApp
-//
-//  Created by Кирилл Нескоромный on 27.10.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGreen
     }
 
 
